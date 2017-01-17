5c30c2bc4274b7f9cd2ca37151210e6c9106e2c3
Dhi Aurrahman <dio@hooq.tv>
2017-01-18 06:53:04 +0700

    Add renderError as an option

--- example.js
+++ example.js
@@ -1,6 +1,18 @@
 const API = require('./')
 const {HttpError} = API
 
+const options = {
+  // HttpError
+  renderError: (err) => {
+    return {
+      errors: [{
+        message: err.message,
+        code: err.internal || '1195'
+      }]
+    }
+  }
+}
+
 module.exports = API([
   {
     method: 'GET',
@@ -13,7 +25,7 @@ module.exports = API([
     method: 'GET',
     path: '/',
     handler: async (req, res) => {
-      throw new HttpError('HAHA', 400)
+      throw new HttpError('HAHA', 400, '1201')
     }
   }
-])
+], options)
