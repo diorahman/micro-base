3ece582d1ddef673cf09dca0ac4afe5e5c594f3d
Dhi Aurrahman <dio@hooq.tv>
2017-01-18 06:53:04 +0700

    Add renderError as an option

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const create = require('uniloc')
-const {json} = require('micro')
+const {json, send} = require('micro')
 const id = require('shortid')
+
+const DEV = process.env.NODE_ENV === 'development'
 
 async function unhandled () { throw new Error('Unhandled') }
 
 class HttpError extends Error {
-  constructor (message, statusCode) {
+  constructor (message, statusCode, internal) {
     super(message)
     this.name = 'HttpError'
+    this.internal = internal
     this.statusCode = statusCode
   }
 }
@@ -15,7 +18,7 @@ class HttpError extends Error {
 module.exports = exports = serve
 exports.HttpError = HttpError
 
-function serve (routes) {
+function serve (routes, settings = {}) {
   const prepared = {}
   const handlers = {}
   for (let route of routes) {
@@ -32,6 +35,27 @@ function serve (routes) {
       req.body = await json(req)
     }
     req.options = options
-    return await handlers[name](req, res)
+    try {
+      return await handlers[name](req, res)
+    } catch (err) {
+      const {renderError} = settings
+      if (typeof renderError === 'function') {
+        let message = renderError(err)
+        if (DEV) {
+          if (typeof message === 'object') {
+            if (Array.isArray(message)) {
+              message.push({stack: err.stack})
+            } else {
+              message.stack = err.stack
+            }
+          } else {
+            message += ' TRACE:' + err.stack
+          }
+        }
+        return send(res, err.statusCode || 500, message)
+      }
+
+      throw err
+    }
   }
 }
